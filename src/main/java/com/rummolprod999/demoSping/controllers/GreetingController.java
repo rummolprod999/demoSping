8ee8a872c3ca9e9152f688e651b68bf5ac258e86
rummolprod999 <rummolprod999@gmail.com>
2020-02-11 12:01:42 +0300

    add controller methods

--- src/main/java/com/rummolprod999/demoSping/controllers/GreetingController.java
+++ src/main/java/com/rummolprod999/demoSping/controllers/GreetingController.java
@@ -2,6 +2,7 @@ package com.rummolprod999.demoSping.controllers;
 
 import com.rummolprod999.demoSping.dao.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,9 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping(
+            value = "/greeting",
+            method = {RequestMethod.GET})
     public Greeting greeting(
             @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
--- src/main/java/com/rummolprod999/demoSping/controllers/HelloController.java
+++ src/main/java/com/rummolprod999/demoSping/controllers/HelloController.java
@@ -1,12 +1,15 @@
 package com.rummolprod999.demoSping.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping(
+            value = "/",
+            method = {RequestMethod.GET})
     public String index() {
         return "Greetings from Spring Boot!";
     }
